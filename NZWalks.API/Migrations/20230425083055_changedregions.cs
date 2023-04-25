0b8e2283588f3e0355ea25328309b4002d933a39
Nikhitha <nikitha.kunapareddy@sailssoftware.com>
2023-04-25 15:44:22 +0530

    Add project files.

---
+++ NZWalks.API/Migrations/20230425083055_changedregions.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NZWalks.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class changedregions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("14ceba71-4b51-4777-9b17-46602cf66153"),
+                columns: new[] { "Code", "Name" },
+                values: new object[] { "BOP", "Bay Of Plenty" });
+
+            migrationBuilder.UpdateData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("6884f7d7-ad1f-4101-8df3-7a6fa7387d81"),
+                columns: new[] { "Code", "Name" },
+                values: new object[] { "NTL", "Northland" });
+
+            migrationBuilder.UpdateData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("906cb139-415a-4bbb-a174-1a1faf9fb1f6"),
+                columns: new[] { "Code", "Name" },
+                values: new object[] { "NSN", "Nelson" });
+
+            migrationBuilder.UpdateData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("cfa06ed2-bf65-4b65-93ed-c9d286ddb0de"),
+                columns: new[] { "Code", "Name" },
+                values: new object[] { "WGN", "Wellington" });
+
+            migrationBuilder.UpdateData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("f077a22e-4248-4bf6-b564-c7cf4e250263"),
+                columns: new[] { "Code", "Name" },
+                values: new object[] { "STL", "Southland" });
+
+            migrationBuilder.UpdateData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("f7248fc3-2585-4efb-8d1d-1c555f4087f6"),
+                columns: new[] { "Code", "Name" },
+                values: new object[] { "AKL", "Auckland" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("14ceba71-4b51-4777-9b17-46602cf66153"),
+                columns: new[] { "Code", "Name" },
+                values: new object[] { "EE", "Eluru" });
+
+            migrationBuilder.UpdateData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("6884f7d7-ad1f-4101-8df3-7a6fa7387d81"),
+                columns: new[] { "Code", "Name" },
+                values: new object[] { "VSKP", "Visakhapatnam" });
+
+            migrationBuilder.UpdateData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("906cb139-415a-4bbb-a174-1a1faf9fb1f6"),
+                columns: new[] { "Code", "Name" },
+                values: new object[] { "RJY", "Rajahmandry" });
+
+            migrationBuilder.UpdateData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("cfa06ed2-bf65-4b65-93ed-c9d286ddb0de"),
+                columns: new[] { "Code", "Name" },
+                values: new object[] { "YLM", "Yalamanchili" });
+
+            migrationBuilder.UpdateData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("f077a22e-4248-4bf6-b564-c7cf4e250263"),
+                columns: new[] { "Code", "Name" },
+                values: new object[] { "SEC", "Secendrabad" });
+
+            migrationBuilder.UpdateData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("f7248fc3-2585-4efb-8d1d-1c555f4087f6"),
+                columns: new[] { "Code", "Name" },
+                values: new object[] { "BZW", "Vijayawada" });
+        }
+    }
+}
